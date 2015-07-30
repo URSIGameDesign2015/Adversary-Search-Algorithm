1be8c73cfd31d3431074268c181ee8aee5866734
Kathryn Hodge <kahodge@vassar.edu>
2015-07-30 11:41:48 -0400

    Framework Set Up to Add in XML Read

--- RegularLanguage-GamePlay/Assets/Scripts/SetUpLevel.cs
+++ RegularLanguage-GamePlay/Assets/Scripts/SetUpLevel.cs
@@ -21,33 +21,40 @@ public class SetUpLevel : MonoBehaviour {
 		// COMP --> "a, b"
 		// PLAYER --> "c ,d"
 		// (a | b) + (ca | bd) + (c | d)
-		
-		// Load XML File: 
-		xmlDoc = XDocument.Load ("/Users/kathrynhodge/GitHub/Game-Design-via-Regular-Language/RegularLanguage-GamePlay/Assets/strategy.xml");
+		// A --> Game Manager makes blue blocks closer
+		// B --> Drama Manager makes purple blocks closer
+		// C --> Player chooses blue block
+		// D --> Player chooses purple block
 		
 		char gmChoice = 'n'; // haven't made one yet
 		// our language instance is from the player --> it's 
 		// static so we can use it here and transfer between levels
 
-		// If it's the beginning, then the GM can make any choice --> ie do nothing
-	     gmChoice = 'a';
+		// If it's the beginning, then the GM can make any choice --> a random choice
 		if (ThirdPersonUserControl.langInstance.Length == 0) {
-//			System.Random rand = new System.Random ();
-//			if (rand.Next (0, 2) == 0) {
-//				// Drama Manager should move blue closer
-//				gmChoice = 'a';
-//			} else {
-//				// Drama Manger should move purple closer
-//				gmChoice = 'b';
-//			}
+			System.Random rand = new System.Random ();
+			if (rand.Next (0, 2) == 0) {
+				// Drama Manager should move blue closer
+				gmChoice = 'a';
+			} else {
+				// Drama Manger should move purple closer
+				gmChoice = 'b';
+			}
 //		// Otherwise it should follow the player's last choice
 		} else {
+			// Load XML File: 
+			xmlDoc = XDocument.Load ("/Users/kathrynhodge/GitHub/Game-Design-via-Regular-Language/RegularLanguage-GamePlay/Assets/strategy.xml");
+
 			// Get player's last choice --> last character put into the language
 			char playerChoice = ThirdPersonUserControl.langInstance [ThirdPersonUserControl.langInstance.Length - 1];
 			// Find out GM's choice by using player's choice and current langInstance
 			// in the strategy file
-
+			
 			// ~~~~READ XML FILE~~~~~~~
+//				for (int i = 0; i < ThirdPersonUserControl.langInstance) {
+//					char elt = xmlDoc.Element("ComputerStrategy")
+//				}
+			
 			
 			// gmChoice = whatever the strategy returns
 			// If strat says GM do a --> make blue blocks closer
